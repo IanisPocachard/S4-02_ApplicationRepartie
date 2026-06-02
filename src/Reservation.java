1545e674b7de06d401cb8440fae193b1d1763685
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2026-06-02 08:56:31 +0200

    Entités base de données

---
+++ src/Reservation.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Reservation {
+
+    private int id;
+    private String nom_client, prenom_client, numero_telephone;
+    private int nbPersonnes;
+    private Restaurant restaurant;
+    private Date date;
+
+
+
+}
---
+++ src/Restaurant.java
@@ -0,0 +1,60 @@
+public class Restaurant {
+
+    private int id;
+    private String nom, adresse;
+    private double latitude, longitude;
+
+    public Restaurant(
+            int id,
+            String nom,
+            String adresse,
+            double latitude,
+            double longitude
+    ) {
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
