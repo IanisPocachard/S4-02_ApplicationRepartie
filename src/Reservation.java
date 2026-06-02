47482fa649db88800050ac92ba7f184e5688e62e
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2026-06-02 08:57:32 +0200

    fix entité Reservation

--- src/Reservation.java
+++ src/Reservation.java
@@ -8,6 +8,59 @@ public class Reservation {
     private Restaurant restaurant;
     private Date date;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getNom_client() {
+        return nom_client;
+    }
+
+    public void setNom_client(String nom_client) {
+        this.nom_client = nom_client;
+    }
+
+    public String getPrenom_client() {
+        return prenom_client;
+    }
+
+    public void setPrenom_client(String prenom_client) {
+        this.prenom_client = prenom_client;
+    }
+
+    public String getNumero_telephone() {
+        return numero_telephone;
+    }
+
+    public void setNumero_telephone(String numero_telephone) {
+        this.numero_telephone = numero_telephone;
+    }
+
+    public int getNbPersonnes() {
+        return nbPersonnes;
+    }
+
+    public void setNbPersonnes(int nbPersonnes) {
+        this.nbPersonnes = nbPersonnes;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
