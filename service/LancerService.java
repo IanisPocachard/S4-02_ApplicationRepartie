cd57fabf04e035fb42fac67a8d028372f3d71aec
Adrien Tritarelli <atritarelli@outlook.com>
2026-06-03 15:54:47 +0200

    modif service incident que ambroise va appeler dans son proxy

--- service/LancerService.java
+++ service/LancerService.java
@@ -6,15 +6,18 @@ import java.rmi.RemoteException;
 public class LancerService {
        public static void main(String[] args) {
         try {
+            // instanciation du service de données
             ServiceDonneesOuvertes service = new ServiceDonneesOuvertes();
 
-            Object serviceExporte = UnicastRemoteObject.exportObject(service, 0);
-            // Enregistrement dans le registre RMI
-            Registry registry = LocateRegistry.createRegistry(1099);
+            // exportation de l'objet service pour le rendre accessible à distance
+            Object rd = (InterfaceIncident) UnicastRemoteObject.exportObject(service, 0);
 
-            InterfaceProxy rd = (InterfaceProxy) serviceExporte;
-            registry.rebind("ServiceDonneesBloquees", rd);
-            System.out.println("Service enregistré dans le registre RMI");
+            // Enregistrement dans le registre RMI, pour le moment on ne spécifie pas de nom pour le service, on le fera plus tard donc sans paramètre ça va chercher sur localhost et sur le port par défaut 1099
+            Registry registry = LocateRegistry.getRegistry();
+
+            InterfaxeProxy serviceDistant = (InterfaxeProxy) registry.lookup("proxy");
+
+            serviceDistant.setIncidents(rd);
             
         } catch (RemoteException e) {
             System.err.println("Erreur RMI : " + e.getMessage());
