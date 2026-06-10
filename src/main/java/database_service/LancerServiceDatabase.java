b8754f267f8aa7c8449cc0f667696bd7a8625bf6
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2026-06-10 13:33:55 +0200

    fonctionnel proxy et database

--- src/main/java/database_service/LancerServiceDatabase.java
+++ src/main/java/database_service/LancerServiceDatabase.java
@@ -1,6 +1,11 @@
 package database_service;
 
+import proxy.InterfaceProxy;
+import proxy.Proxy;
+
 import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -22,23 +27,44 @@ public class LancerServiceDatabase {
         }
 
         Database database = Database.getInstance(Credentials.USERNAME, Credentials.PASSWORD);
+
+        System.out.println("[DATABASE] Réinitialisation des tables...");
+        Reservation.dropAll();
+        System.out.println("[DATABASE] Table Reservation réinitialisée.");
+        TableRestaurant.dropAll();
+        System.out.println("[DATABASE] Table TableRestaurant réinitialisée.");
+        Restaurant.dropAll();
+        System.out.println("[DATABASE] Table Restaurant réinitialisée.");
+
+        System.out.println("\n[DATABASE] Remplissage des tables...");
         try {
             database.chargerRestaurants(jsonFileRestaurant);
+            System.out.println("[DATABASE] Table Restaurant peuplée.");
         } catch (IOException e) {
-            System.err.println("Erreur : fichier non trouvé " + jsonFileRestaurant + "\n" + e.getMessage());
+            System.err.println("[DATABASE] Erreur : fichier non trouvé " + jsonFileRestaurant + "\n" + e.getMessage());
         }
         try {
             database.chargerTables(jsonFileTables);
+            System.out.println("[DATABASE] Table TableRestaurant peuplée.");
         } catch (IOException e) {
-            System.err.println("Erreur : fichier non trouvé " + jsonFileTables + "\n" + e.getMessage());
+            System.err.println("[DATABASE] Erreur : fichier non trouvé " + jsonFileTables + "\n" + e.getMessage());
         }
 
         try {
             ServiceDatabase rd = (ServiceDatabase) UnicastRemoteObject.exportObject((ServiceDatabase) database, 0);
             Registry registry = LocateRegistry.getRegistry(1099);
-            registry.rebind("database", rd);
-        } catch (Exception e) {
+
+            // Lier le service Database au proxy
+            InterfaceProxy serviceProxy = (InterfaceProxy) registry.lookup("proxy");
+            serviceProxy.setRestaurant(rd);
+            System.out.println("[DATABASE] Service Database lié au proxy !");
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
             e.printStackTrace();
         }
+
+        System.out.println("\n[DATABASE] Service Database lancé !");
     }
 }
