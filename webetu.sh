2335ec422a273a46588f203544e3e721f44a9d66
Ambroise Gilbert <e63345u@iutnc-125-04.ad.univ-lorraine.fr>
2026-06-10 13:51:11 +0200

    modif du bash

--- webetu.sh
+++ webetu.sh
@@ -4,4 +4,8 @@ cd frontend/
 
 nmp run build
 
-zip 
+zip -r -9 -e carte.zip  css/ pages/ dist/
+
+echo "..." | scp carte.zip ...@webetu.iutnc.univ_lorraine.fr:/www
+
+echo "..." | ssh  ...@webetu.iutnc.univ_lorraine.fr "cd /www && unzip carte.zip"
