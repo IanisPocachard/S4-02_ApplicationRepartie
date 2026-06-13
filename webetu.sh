f67c00de85e765fee683f27657fa42af08be9dc2
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2026-06-13 16:19:29 +0200

    modif script pour déploiement automatique de la page web sur webetu

--- webetu.sh
+++ webetu.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+set -e # stop le script entier si une seule commande échoue
 
-cd frontend/
+USER_WEBETU="e71517u"
+LIEN_WEBETU="webetu.iutnc.univ-lorraine.fr"
+WEBETU_DIR="www/"
+ARCHIVE="carte.zip"
 
-nmp run build
+cd "$(dirname "$0")/frontend" # se place à la racine du projet
 
-zip -r -9 -e carte.zip  css/ pages/ dist/
+npm run build  # compilation du typescript
 
-echo "..." | scp carte.zip ...@webetu.iutnc.univ_lorraine.fr:/www
+rm -f "$ARCHIVE" # supprime l'ancienne archive au cas où elle soit restée à cause d'un crash du programme
 
-echo "..." | ssh  ...@webetu.iutnc.univ_lorraine.fr "cd /www && unzip carte.zip"
+zip -r -9 "$ARCHIVE" \
+  index.html \
+  config.json \
+  css \
+  pages \
+  dist
+
+scp "$ARCHIVE" "${USER_WEBETU}@${LIEN_WEBETU}:${WEBETU_DIR}/"
+
+ssh "${USER_WEBETU}@${LIEN_WEBETU}" "
+  cd ${WEBETU_DIR} &&
+  rm -rf css pages dist index.html config.json &&
+  unzip -o ${ARCHIVE} &&
+  rm ${ARCHIVE}
+"
+
+rm -f "$ARCHIVE" # supprime l'archive
+
+echo "Déploiement terminé."
